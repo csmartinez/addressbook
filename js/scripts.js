98f1f53a2fcf2164c702ef10e1131f032d273ffd
Monty <nathanarohde@gmail.com>
2015-02-17 11:50:11 -0800

    Seperated address into 4 different fields.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,51 @@
 $(document).ready(function() {
-  $('form#new-contact').submit(function(event) {
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
+    
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedStreet1 = $("input#new-street1").val();
+    var inputtedStreet2 = $("input#new-street2").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val()
 
-    var inputFirstName = $('input#new-first-name').val();
-    var inputLastName = $('input#new-last-name').val();
-    var inputAddress = $('input#new-address').val();
-    var newContact = { firstName: inputFirstName, lastName: inputLastName, address: inputAddress };
+    var newAddress = { street1: inputtedStreet1,
+      street2: inputtedStreet2,
+      city: inputtedCity,
+      state: inputtedState,
+      zip: inputtedZip,
+      fullAddress: function() {
+        if ((/([\w])/).test(this.street2)) {
+          return this.street1 + ', ' + this.street2 + ', ' + this.city + ', ' + this.state + ', ' + this.zip;
+        } else {
+          return this.street1 + ', ' + this.city + ', ' + this.state + ', ' + this.zip;
+        }
+      }
+    };
 
-    $('ul#contacts').append('<li><span class="contact">' + newContact.firstName + " " + newContact.lastName + '</span></li>');
+    var newContact = { firstName: inputtedFirstName,
+      lastName: inputtedLastName,
+      address: newAddress.fullAddress()
+    };
 
-    $('input#new-first-name').val("");
-    $('input#new-last-name').val("");
-    $('input#new-address').val("");
+    $("ul#contacts").append("<li><span class='contact'>" +
+    newContact.firstName + " " +
+    newContact.lastName +
+    "</span></li>");
 
-    $('.contact').last().click(function() {
-      $('#show-contact').show();
-      $('#show-contact h2').text(newContact.firstName + " " + newContact.lastName);
-      $('.first-name').text(newContact.firstName);
-      $('.last-name').text(newContact.lastName);
-      $('.address').text(newContact.address);
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-street1").val("");
+    $("input#new-street2").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + "" + newContact.lastName);
+      $(".address").text(newContact.address);
     });
   });
 });
